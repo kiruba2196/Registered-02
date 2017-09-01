83368451cbc16880ad0c266992da8ab77ddfeef4
kiruba2196 <kiruba2196@gmail.com>
2017-09-01 22:07:58 +0530

    Create DigitReverse.java

---
+++ DigitReverse.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+    public class DigitReverse
+    {
+        public static void main(String args[])
+        {
+        	Scanner sc=new Scanner(System.in);
+        	String st=sc.next();
+        	StringBuffer sb=new StringBuffer(st);
+        	System.out.println(sb.reverse());
+        		
+        }
+    }
